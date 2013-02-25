ae06f2911f5a33fa25fb8a41785c892ca8c6f484
Vasili Gulevich <V.V.Gulevich@mail.ru>
2013-02-26 00:24:37 +0700

    Rename support
    Configuration removal events are handled and interpreted to keep changed
    references intact. Untested.

--- src/multilauncher/MultiLaunch.java
+++ src/multilauncher/MultiLaunch.java
@@ -1,74 +1,24 @@
 package multilauncher;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Set;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
 
 public class MultiLaunch implements ILaunchConfigurationDelegate {
 	static final String sequenceFieldName = "sequence";
-	public static Dictionary<String, ILaunchConfiguration> getValidConfigurations(ILaunchConfiguration current) throws CoreException
-	{
-		//TODO: consider another key to persist configuration association on rename
-		//TODO: deal with raw types
-		ILaunchConfiguration[] configurations = getLaunchManager().getLaunchConfigurations();
-		Hashtable<String, ILaunchConfiguration> rv = new Hashtable<>(configurations.length);
-		@SuppressWarnings("rawtypes")
-		Set requiredModes = current.getModes(); // Actually just run and debug
-		for (ILaunchConfiguration iLaunchConfiguration : configurations) {				
-			//TODO: Prevent configuration cycles (deep detection)
-			if (iLaunchConfiguration.contentsEqual(current))
-				continue;
-			@SuppressWarnings("rawtypes")
-			Set modes= iLaunchConfiguration.getModes();
-			if (!modes.containsAll(requiredModes))
-				continue;
-			
-			//TODO: More filters are probably needed here
-			//WARN: relies on unique configuration naming (provided by IDE)
-			rv.put(iLaunchConfiguration.getName(), iLaunchConfiguration);
-		}
-		return rv;
-	}
-	public static Collection<ILaunchConfiguration> getConfigurationsToRun(ILaunchConfiguration current) throws CoreException
-	{
-		//WARN: configuration selection is lost on rename
-		@SuppressWarnings("rawtypes")
-		List sequence = current.getAttribute(MultiLaunch.sequenceFieldName, Collections.emptyList());
-		ArrayList<ILaunchConfiguration> rv = new ArrayList<ILaunchConfiguration>(sequence.size());
-		Dictionary<String, ILaunchConfiguration> configurations = getValidConfigurations(current);
-		for (Object name: sequence) {
-			ILaunchConfiguration configuration = configurations.get(name);
-			if (configuration!=null)
-				rv.add(configuration);
-		}
-		return rv;
-	}
-	
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		//Children have their own launch object. This one is not needed.
-		getLaunchManager().removeLaunch(launch); 
-		for (ILaunchConfiguration toRun : getConfigurationsToRun(configuration)) {
+		MultiLaunchConfiguration.getLaunchManager().removeLaunch(launch); 
+		for (ILaunchConfiguration toRun : MultiLaunchConfiguration.getReferences(configuration)) {
 			//TODO: progress might be shown incorrectly
 			//TODO: we should probably process ILaunch return value somehow
 			toRun.launch(mode, monitor);
 		}
 	}
 
-	protected static ILaunchManager getLaunchManager() {
-		return DebugPlugin.getDefault().getLaunchManager();
-	}
 }
---
+++ src/multilauncher/MultiLaunchConfiguration.java
@@ -0,0 +1,119 @@
+package multilauncher;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchManager;
+
+public class MultiLaunchConfiguration {
+	public static boolean isMultiLaunchConfiguration(ILaunchConfiguration configuration) throws CoreException {
+		return configuration.getType().getIdentifier().equals("Multilauncher.MultiLaunch");
+	}
+	static interface Action {
+		void act(ILaunchConfiguration configuration)  throws CoreException;
+	}
+	public static List<String> getReferencesNames(ILaunchConfiguration configuration) throws CoreException {
+		assert(isMultiLaunchConfiguration(configuration));
+		@SuppressWarnings("rawtypes")
+		List sequence = configuration.getAttribute(MultiLaunch.sequenceFieldName, Collections.emptyList());
+		ArrayList<String> rv = new ArrayList<>(sequence.size());
+		for (Object item : sequence) {
+			rv.add(item.toString());
+		}
+		return rv;
+		
+	}
+	//
+	
+	/**
+	 * All configurations that can be referenced by current
+	 * Complexity: n
+	 * @param current - configuration of type MultiLaunch to analyze
+	 * @return collection of valid references 
+	 * @throws CoreException
+	 */
+	public static Dictionary<String, ILaunchConfiguration> getPossibleReferences(ILaunchConfiguration current) throws CoreException
+	{
+		//TODO: consider another key to persist configuration association on rename
+		//TODO: deal with raw types
+		ILaunchConfiguration[] configurations = getLaunchManager().getLaunchConfigurations();
+		Hashtable<String, ILaunchConfiguration> rv = new Hashtable<>(configurations.length);
+		@SuppressWarnings("rawtypes")
+		Set requiredModes = current.getModes(); // Actually just run and debug
+		for (ILaunchConfiguration iLaunchConfiguration : configurations) {				
+			//TODO: Prevent configuration cycles (deep detection)
+			if (iLaunchConfiguration.contentsEqual(current))
+				continue;
+			@SuppressWarnings("rawtypes")
+			Set modes= iLaunchConfiguration.getModes();
+			if (!modes.containsAll(requiredModes))
+				continue;
+			
+			//TODO: More filters are probably needed here
+			//WARN: relies on unique configuration naming (provided by IDE)
+			rv.put(iLaunchConfiguration.getName(), iLaunchConfiguration);
+		}
+		return rv;
+	}
+	
+	
+	/**
+	 * Configurations referenced by current
+	 * @param current -  configuration of type MultiLaunch to analyze
+	 * @return collection of active references
+	 * @throws CoreException
+	 */
+	public static Collection<ILaunchConfiguration> getReferences(ILaunchConfiguration current) throws CoreException
+	{
+		//WARN: configuration selection is lost on rename
+		Collection<String> sequence = getReferencesNames(current);
+		ArrayList<ILaunchConfiguration> rv = new ArrayList<ILaunchConfiguration>(sequence.size());
+		Dictionary<String, ILaunchConfiguration> configurations = getPossibleReferences(current);
+		for (String name: sequence) {
+			ILaunchConfiguration configuration = configurations.get(name);
+			if (configuration!=null)
+				rv.add(configuration);
+		}
+		return rv;
+	}
+	
+	public static Dictionary<String, ILaunchConfiguration> getAll() throws CoreException {
+		Dictionary<String, ILaunchConfiguration> rv = new Hashtable<String, ILaunchConfiguration>();
+		ILaunchConfiguration[] configurations = MultiLaunchConfiguration.getLaunchManager().getLaunchConfigurations();
+		for (ILaunchConfiguration iLaunchConfiguration : configurations) {
+			//TODO: ensure that id comparison is enough to select configurations of our type
+			if (!MultiLaunchConfiguration.isMultiLaunchConfiguration(iLaunchConfiguration))
+				continue;
+			rv.put(iLaunchConfiguration.getName(), iLaunchConfiguration);
+		}
+		return rv;
+	}
+
+	public static void scanAll(Action action) throws CoreException {
+		Dictionary<String, ILaunchConfiguration> ourConfigurations = MultiLaunchConfiguration.getAll();
+		Enumeration<ILaunchConfiguration> enumeration = ourConfigurations.elements();
+		scan(action, enumeration);
+	}
+	public static void scan(Action action, Enumeration<ILaunchConfiguration> enumeration) throws CoreException {
+		while (enumeration.hasMoreElements()) {
+			ILaunchConfiguration configuration = enumeration.nextElement();
+			action.act(configuration);
+		}
+	}
+
+	public static ILaunchManager getLaunchManager() {
+		return DebugPlugin.getDefault().getLaunchManager();
+	}
+	
+
+
+}
