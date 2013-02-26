cb1e07903b46ed4de313409328d3f0ad2fc7aab4
Gulevich Vasili <V.V.Gulevich@mail.ru>
2013-02-26 10:27:24 +0700

    Obsolete Dictionary removed.

--- src/multilauncher/Activator.java
+++ src/multilauncher/Activator.java
@@ -1,9 +1,6 @@
 package multilauncher;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
@@ -25,47 +22,44 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
-	 * Watcher for deletions and renames.
-	 * References to renamed configurations are persisted.
-	 * References to deleted configurations are cleaned out. 
+	 * Watcher for deletions and renames. References to renamed configurations
+	 * are persisted. References to deleted configurations are cleaned out.
 	 */
 	final ILaunchConfigurationListener _configurationListener = new ILaunchConfigurationListener() {
-		
+
 		public void updateConfiguration(ILaunchConfiguration configuration, List<String> sequence) throws CoreException {
-			assert(MultiLaunchConfiguration.isMultiLaunchConfiguration(configuration));
+			assert (MultiLaunchConfiguration.isMultiLaunchConfiguration(configuration));
 			ILaunchConfigurationWorkingCopy workingCopy = configuration.getWorkingCopy();
 			workingCopy.setAttribute(MultiLaunch.sequenceFieldName, sequence);
-			//TODO: is it ok not to save parent?
+			// TODO: is it ok not to save parent?
 			workingCopy.doSave();
 		}
-		public void launchConfigurationRenamed(final ILaunchConfiguration movedFrom, final ILaunchConfiguration movedTo)
-		{
+
+		public void launchConfigurationRenamed(final ILaunchConfiguration movedFrom,
+				final ILaunchConfiguration movedTo) {
 			try {
-				MultiLaunchConfiguration.scanAll(new MultiLaunchConfiguration.Action() {
-					@Override
-					public void act(ILaunchConfiguration configuration)  throws CoreException {
-							List<String> sequence = MultiLaunchConfiguration.getReferencesNames(configuration);
-							//TODO: consider smarter in-place replace
-							List<String> newSequence = new ArrayList<String>(sequence.size());
-							Boolean modified = false; 
-							for (String name : sequence) {
-								if (name.equals(movedFrom.getName()))
-									name = movedTo.getName();
-								newSequence.add(name);
-								modified = true;
-							}
-							if (modified)
-								updateConfiguration(configuration, newSequence);
+				for (ILaunchConfiguration configuration : MultiLaunchConfiguration.getAll().values()) {
+					List<String> sequence = MultiLaunchConfiguration.getReferencesNames(configuration);
+					// TODO: consider smarter in-place replace
+					List<String> newSequence = new ArrayList<String>(sequence.size());
+					Boolean modified = false;
+					for (String name : sequence) {
+						if (name.equals(movedFrom.getName()))
+							name = movedTo.getName();
+						newSequence.add(name);
+						modified = true;
 					}
-				});
-			} catch (CoreException e)
-			{
-				//TODO: consider better exception handling
+					if (modified)
+						updateConfiguration(configuration, newSequence);
+				}
+			} catch (CoreException e) {
+				// TODO: consider better exception handling
 				DebugPlugin.log(e);
 			}
 		}
+
 		@Override
 		public void launchConfigurationRemoved(final ILaunchConfiguration configuration) {
 			ILaunchConfiguration movedTo = MultiLaunchConfiguration.getLaunchManager().getMovedTo(configuration);
@@ -73,27 +67,26 @@ public class Activator extends AbstractUIPlugin {
 				launchConfigurationRenamed(configuration, movedTo);
 				return;
 			}
-			//when referenced configuration is removed, we should ensure that another configuration with same name won't become referenced instead
+			// when referenced configuration is removed, we should ensure that
+			// another configuration with same name won't become referenced
+			// instead
 			try {
-				MultiLaunchConfiguration.scanAll(new MultiLaunchConfiguration.Action() {
-					@Override
-					public void act(ILaunchConfiguration cleanupCandidate)  throws CoreException {
-						List<String> newSequence = MultiLaunchConfiguration.getReferencesNames(cleanupCandidate);
-						if (newSequence.remove(configuration.getName()))
-							updateConfiguration(cleanupCandidate, newSequence);
-					}
-				});
-			} catch (CoreException e)
-			{
-				//TODO: consider better exception handling
+				for (ILaunchConfiguration cleanupCandidate : MultiLaunchConfiguration.getAll().values()) {
+					List<String> newSequence = MultiLaunchConfiguration.getReferencesNames(cleanupCandidate);
+					if (newSequence.remove(configuration.getName()))
+						updateConfiguration(cleanupCandidate, newSequence);
+
+				}
+			} catch (CoreException e) {
+				// TODO: consider better exception handling
 				DebugPlugin.log(e);
 			}
 		}
-		
+
 		@Override
 		public void launchConfigurationChanged(ILaunchConfiguration configuration) {
 		}
-		
+
 		@Override
 		public void launchConfigurationAdded(ILaunchConfiguration configuration) {
 		}
@@ -107,27 +100,35 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		MultiLaunchConfiguration.getLaunchManager().addLaunchConfigurationListener(_configurationListener);
+		MultiLaunchConfiguration.getLaunchManager().addLaunchConfigurationListener(
+				_configurationListener);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
-		MultiLaunchConfiguration.getLaunchManager().removeLaunchConfigurationListener(_configurationListener);
+		MultiLaunchConfiguration.getLaunchManager().removeLaunchConfigurationListener(
+				_configurationListener);
 		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -135,17 +136,15 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
-	
-
-	
 
 }
